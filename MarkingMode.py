4e36fded7dd3599ec9c7f6b9f9e501209d80826e
Dex-4945 <92041563+Dex-4945@users.noreply.github.com>
2023-10-25 11:56:29 -0700

    Update MarkingMode.py
    finish math function

--- MarkingMode.py
+++ MarkingMode.py
@@ -14,25 +14,40 @@ if ("""button unchecked"""):
 if ("""button trigger"""):
   markME(evalMode, answers, questions);
 
+
+
+
+
+
+
 #my function
 #Function calculates points 
-def doTheMath(mode, question, answer):
+def doTheMath(mode, question, answer, totalQ, totalA):
   isCorrect == False
   if answer >= 0:
     isCorrect = question[1][answer][2];
   if isCorrect:
-    if mode == 1:
-      if isCorrect
+    if mode == 1 or mode == 2:
+      return 1
+    if mode == 3:
+      return ((totalA/totalQ) - 1)
+  else:
+    if mode == 2 or mode == 3:
+      return -1
   return 0
 #Stores points according to each marking method in list
 def markMe(evalMode, answers, questions):
   #marks[] contains three values, one for each marking method
   marks = []
-  for i in questions:
+  totalQ = len(questions)
+  totalA = 0
+  for q in range(len(questions)):
+    totalA += len(questions[q][1])
+  for i in range(len(questions)):
     #evalMode 1
     marks[0] += doTheMath(1, questions[i], answers[i])
     #evalMode 2
     marks[1] += doTheMath(2, questions[i], answers[i])
     #evalMode 3
-    marks[2] += doTheMath(3, questions[i], answers[i])
+    marks[2] += doTheMath(3, questions[i], answers[i], totalQ, totalA)
   return marks
