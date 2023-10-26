65f0b29eae95bd268f18b2b6e0a200e93aab074e
Dex-4945 <92041563+Dex-4945@users.noreply.github.com>
2023-10-26 12:21:41 +0200

    correction
    same result, but corrected

--- Visualisateur.py
+++ Visualisateur.py
@@ -1,78 +1,48 @@
 from tkinter import *
 from tkinter import ttk
+import qcm
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-
-
 class Question():
-    name = ""
-    reponse = []
-    Choix = 0
-    def __init__(self, name, reponse):
-        name = self.name
-        reponse = self.reponse
-        return
-
-class Questionnaire():
-    listQuestion = []
-
-    index = 0
-
-    def __init__(self):
-        ttk.Button(frm, text="<-", command=self.PrevQuestion).grid(column=0, row=0)
-        ttk.Button(frm, text="->", command=self.NextQuestion).grid(column=2, row=0)
+    def __init__(self, questionLine):
+        self.name = questionLine[0]
+        self.answers = questionLine[1]
+        self.choices = len(self.answers)
 
 
-        self.ReadFile("QCM.txt")
-        self.ShowQuestion(self.index)
+questionnaire = qcm.build_questionnaire('QCM.txt')
+questions = []
+for i in range(len(questionnaire)):
+    questions.append(Question(questionnaire[i]))
+progress = 0
+print("hello")
 
+def nextQuestion():
+    global progress
+    if(progress + 1 < len(questions)):
+        progress += 1
+        print(progress)
+    showQuestion()
 
-        print(self.listQuestion[1].name)
+def previousQuestion():
+    global progress
+    if(progress != 0):
+        progress -= 1
+        print(progress)
+    showQuestion()
 
+def showQuestion():
+    for widget in frm.winfo_children():
+        widget.grid_remove()
+    ttk.Button(frm, text="<--", command=previousQuestion).grid(column=0, row=0)
+    ttk.Button(frm, text="-->", command=nextQuestion).grid(column=2, row=0)
+    ttk.Label(frm, text=questions[progress].name).grid(column=1, row=1)
+    for i in range(questions[progress].choices):
+        ttk.Label(frm, text=questions[progress].answers[i][0]).grid(column=1, row=i+2)
 
-        return
-
-    def NextQuestion(self):
-        self.index+=1
-        self.ShowQuestion(self.index)
-    def PrevQuestion(self):
-        if(self.index !=0):
-            self.index-=1
-        self.ShowQuestion(self.index)
-
-
-    def ShowQuestion(self, index):
-        ttk.Label(frm, text=self.listQuestion[index].name).grid(column=1, row=0)
-        for i in range(len(self.listQuestion[index].reponse)):
-            ttk.Label(frm, text=self.listQuestion[index].reponse[i]).grid(column=1, row=i+1)
-
-
-
-    def ReadFile(self, filename):
-        f = open(filename, "r")
-        q = Question("name", "question")
-        for line in f:
-            if(line.startswith("Q")):
-                q.name = line
-            elif(line.startswith("A")):
-                q.reponse.append(line)
-            else:
-                self.listQuestion.append(q)
-                print(q.name)
-
-
-            
-            
-
-                
-
-
-
-questionnaire = Questionnaire()
-
-
-
-
+ttk.Button(frm, text="<--", command=previousQuestion).grid(column=0, row=0)
+ttk.Button(frm, text="-->", command=nextQuestion).grid(column=2, row=0)
+showQuestion()
 
 root.mainloop()
