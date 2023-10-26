6495fc0acafbeed1524fd49b85e7070901439a3a
Dex-4945 <92041563+Dex-4945@users.noreply.github.com>
2023-10-26 08:46:18 +0200

    Update MarkingMode.py
    optimise

--- MarkingMode.py
+++ MarkingMode.py
@@ -21,34 +21,26 @@ if ("""button trigger"""):
 
 
 #my function
-#Function calculates points 
-def doTheMath(mode, question, answer, totalQ, totalA):
-  isCorrect == False
-  if answer >= 0:
-    isCorrect = question[1][answer][2];
-  if isCorrect:
-    if mode == 1 or mode == 2:
-      return 1
-    if mode == 3:
-      return ((totalA/totalQ) - 1)
-  else:
-    if mode == 2 or mode == 3:
-      return -1
-  return 0
-  
-#Stores points according to each marking method in list
+#Stores points in list and sums them up according to each marking method.
 def markMe(answers, questions):
   #marks[] contains three values, one for each marking method
-  marks = []
   totalQ = len(questions)
   totalA = 0
   for q in range(len(questions)):
     totalA += len(questions[q][1])
-  for i in range(len(questions)):
-    #evalMode 1
-    marks[0] += doTheMath(1, questions[i], answers[i])
-    #evalMode 2
-    marks[1] += doTheMath(2, questions[i], answers[i])
-    #evalMode 3
-    marks[2] += doTheMath(3, questions[i], answers[i], totalQ, totalA)
+  print(str(totalA) + " " + str(totalQ))
+  marks = [[0, 0, 0], []]
+  for q in range(len(questions)):
+    isCorrect = False
+    if answers[q] >= 0:
+      isCorrect = questions[1][answers[q]][2];
+    if isCorrect:
+      marks[0][0] += 1
+      marks[0][1] += 1
+      marks[0][2] += ((totalA/totalQ) - 1)
+      marks[1].append(True)
+    else:
+      marks[0][1] -= 1
+      marks[0][2] -= 1
+      marks[1].append(False)
   return marks
